80c70ceb729a210f95b71716f1659c7a987a241f
Jmart5564 <Jmart5564@gmail.com>
2022-10-03 14:38:15 -0700

    added CSS using styled components to an H! element

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Chart from './components/Chart/Chart';
 import Table from './components/Table/Table';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
@@ -12,9 +19,11 @@ function App() {
       </nav>
       <Switch>
         <Route path="/table" component={Table}>
+          <H1>This is a Table</H1>
           <Table />
         </Route>
         <Route path="/charts" component={Chart}>
+          <H1>This is a Chart</H1>
           <Chart />
         </Route>
       </Switch>
