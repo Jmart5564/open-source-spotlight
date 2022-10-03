4464b56b54c00ecb883e61f42d8d118a61a1c893
Jmart5564 <Jmart5564@gmail.com>
2022-10-03 11:12:03 -0700

    table showing more columns of data

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import usersData from '../../users.json';
 import { Table as RTable, Column, HeaderCell, Cell } from 'rsuite-table';
-// import 'rsuite-table/dist/css/rsuite-table.css';
+import 'rsuite-table/dist/css/rsuite-table.css';
 
 export default function Table() {
   const [users] = useState(usersData);
+
+  const ImageCell = ({ rowData, dataKey, ...rest }) => (
+    <Cell {...rest}>
+      <img src={rowData[dataKey]} width="50" />
+    </Cell>
+  );
 
  
   return (
@@ -13,6 +19,29 @@ export default function Table() {
         <HeaderCell>ID</HeaderCell>
         <Cell dataKey="id" />
       </Column> 
+
+
+      <Column width={100} sortable resizable>
+        <HeaderCell>First Name</HeaderCell>
+        <Cell dataKey="first_name" />
+      </Column>
+
+      <Column width={100} sortable resizable>
+        <HeaderCell>Last Name</HeaderCell>
+        <Cell dataKey="last_name" />
+      </Column>
+
+      <Column width={100} sortable resizable>
+        <HeaderCell>Favorite Color</HeaderCell>
+        <Cell dataKey="favorite_color" />
+      </Column>
+
+      <Column width={100} sortable resizable>
+        <HeaderCell>Image</HeaderCell>
+        <ImageCell dataKey="image" />
+      </Column>
     </RTable>
+
+    
   );
 }
